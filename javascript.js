f4e27bbe0d030e053a563312fbdfe47f612becdc
Wilhelm Perdahl <wilhelm.perdahl@gmail.com>
2016-09-01 11:16:10 +0200

    snake is moving

--- javascript.js
+++ javascript.js
@@ -36,16 +36,19 @@ printGameState(game);
 
 function printGameState(game) {
     ctx.fillStyle = "green";
-    ctx.fillRect(game.apple.xcoord, game.apple.ycoord, 10, 5);
+    ctx.fillRect(game.apple.xcoord, game.apple.ycoord, 5, 2);
 
     ctx.fillStyle = "red";
-    ctx.fillRect(game.player.xcoord, game.player.ycoord, 10, 5);
+    ctx.fillRect(game.player.xcoord, game.player.ycoord, 5, 5);
 
-    for (y = 0; y <= canvas.height - 1; y++) {
+ //   ctx.fillStyle = "black";
+//    ctx.fillRect(game.player.xcoord - game.player.momentum[game.momentumKey].x, game.player.ycoord - game.player.momentum[game.momentumKey].y, 5, 5);
+
+    for (var y = 0; y <= canvas.height - 1; y++) {
         ctx.fillRect(0, y, 10, 5);
         ctx.fillRect(canvas.width - 1, y, 10, 5);
     }
-    for (x = 5; x <= canvas.width - 1; x++) {
+    for (var x = 5; x <= canvas.width - 1; x++) {
         ctx.fillRect(x, 0, 10, 5);
         ctx.fillRect(x, canvas.height - 1, 10, 5);
     }
@@ -57,35 +60,31 @@ function initialize(game) {
 function getUserInput() {
 
 }
-function modifyGameState(game) {
+function registerListeners(game) {
     // var key;
     // key =
     $("#game").keydown(function (e) {
-        console.log(e.keyCode);
         key = e.keyCode;
+        if (key === 37) {
+            if (game.momentumKey === 0) {
+                game.momentumKey = 3;
+            } else {
+                game.momentumKey--;
+            }
+        } else if (key === 39) {
+            if (game.momentumKey === 3) {
+                game.momentumKey = 0;
+            } else {
+                game.momentumKey++;
+            }
+        }
     });
-    if (key === 37) {
-        if (game.momentumKey === 0) {
-            game.momentumKey = 3;
-        } else {
-            momentumKey--;
-        }
-    } else if (key === 39) {
-        if (game.momentumKey === 3) {
-            game.momentumKey = 0;
-        } else {
-            momentumKey++;
-            game.gameover = 1;
-        }
-    }
-
-    console.log(key);
-//
 }
 
 function updateGameState(game) {
-    game.player.xcoord = game.player.momentum[game.momentumKey].x;
-    game.player.ycoord = game.player.momentum[game.momentumKey].y;
+    game.player.xcoord += game.player.momentum[game.momentumKey].x;
+    game.player.ycoord += game.player.momentum[game.momentumKey].y;
+
     if (game.player.xcoord === canvas.width - 1 || game.player.xcoord === 0) {
         game.gameover = 1;
     } else if (game.player.ycoord === canvas.height - 1 || game.player.ycoord === 0) {
@@ -104,12 +103,11 @@ function updateGameState(game) {
 // end game grafix ("Game over")
 
 gameLoop(game);
+registerListeners(game);
 function gameLoop(game) {
-    modifyGameState(game);
     updateGameState(game);
     printGameState(game);
-
     if (!game.gameover) {
-        setTimeout(gameLoop, 2000);
+        setTimeout(gameLoop.bind(null,game), 500);
     }
 }
