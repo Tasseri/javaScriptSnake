b59093f97b712422c0b7081c8739e60febef0837
Wilhelm Perdahl <wilhelm.perdahl@gmail.com>
2016-09-22 10:31:22 +0200

    snake 1.0

--- javascript.js
+++ javascript.js
@@ -1,67 +1,63 @@
 var game = {
-    player: {
-        xcoord: 100,
-        ycoord: 100,
-        momentum: [{
-            x: 0,
-            y: -1
-        },
-            {
-                x: 1,
-                y: 0
-            },
-            {
-                x: 0,
-                y: 1
-            },
-            {
-                x: -1,
-                y: 0
-            }]
-    },
+    players: [
+        {
+            player: {
+                xcoord: 200,
+                ycoord: 100
+            }
+        }],
     apple: {
         xcoord: 100,
-        ycoord: 10
+        ycoord: 80
     },
+    momentum: [{
+        x: 0,
+        y: -1
+    },
+        {
+            x: 1,
+            y: 0
+        },
+        {
+            x: 0,
+            y: 1
+        },
+        {
+            x: -1,
+            y: 0
+        }],
     momentumKey: 0,
-    gameover: 0
+    gameover: 0,
+    border: {w : 10, h: 5}
 };
-var key;
-
-// initilize game (grafix and start positions)
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 printGameState(game);
 
 function printGameState(game) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.height, canvas.width);
     ctx.fillStyle = "green";
-    ctx.fillRect(game.apple.xcoord, game.apple.ycoord, 5, 2);
+    ctx.fillRect(game.apple.xcoord, game.apple.ycoord, 1, 1);
 
     ctx.fillStyle = "red";
-    ctx.fillRect(game.player.xcoord, game.player.ycoord, 5, 5);
-
- //   ctx.fillStyle = "black";
-//    ctx.fillRect(game.player.xcoord - game.player.momentum[game.momentumKey].x, game.player.ycoord - game.player.momentum[game.momentumKey].y, 5, 5);
+    for (var i = 0; i < game.players.length; i++) {
+        ctx.fillRect(game.players[i].player.xcoord, game.players[i].player.ycoord, 1, 1);
+    }
 
     for (var y = 0; y <= canvas.height - 1; y++) {
-        ctx.fillRect(0, y, 10, 5);
-        ctx.fillRect(canvas.width - 1, y, 10, 5);
+        ctx.fillRect(0, y, game.border.w, game.border.h);
+        ctx.fillRect(canvas.width - game.border.w, y, game.border.w, game.border.h);
     }
     for (var x = 5; x <= canvas.width - 1; x++) {
-        ctx.fillRect(x, 0, 10, 5);
-        ctx.fillRect(x, canvas.height - 1, 10, 5);
+        ctx.fillRect(x, 0, game.border.w, game.border.h);
+        ctx.fillRect(x, canvas.height - game.border.h, game.border.w, game.border.h);
     }
 }
-function initialize(game) {
-    // g
-    math.random()
-}
-function getUserInput() {
-
-}
 function registerListeners(game) {
-    // var key;
+    var key;
     // key =
     $("#game").keydown(function (e) {
         key = e.keyCode;
@@ -82,23 +78,38 @@ function registerListeners(game) {
 }
 
 function updateGameState(game) {
-    game.player.xcoord += game.player.momentum[game.momentumKey].x;
-    game.player.ycoord += game.player.momentum[game.momentumKey].y;
+    var newplayer = {player: {xcoord: 0, ycoord: 0}};
+    newplayer.player.xcoord = game.momentum[game.momentumKey].x;
+    newplayer.player.ycoord = game.momentum[game.momentumKey].y;
+    newplayer.player.xcoord += game.players[game.players.length - 1].player.xcoord;
+    newplayer.player.ycoord += game.players[game.players.length - 1].player.ycoord;
+    game.players.push(newplayer);
+    var head = game.players.length - 1;
 
-    if (game.player.xcoord === canvas.width - 1 || game.player.xcoord === 0) {
+    if (game.players[head].player.xcoord === canvas.width - game.border.w || game.players[head].player.xcoord === game.border.w) {
         game.gameover = 1;
-    } else if (game.player.ycoord === canvas.height - 1 || game.player.ycoord === 0) {
+    } else if (game.players[head].player.ycoord === canvas.height - game.border.h || game.players[head].player.ycoord === game.border.h) {
         game.gameover = 1;
     }
+    if (head - 2 > 0) {
+        for (var i = 0; i < game.players.length - 2; i++) {
+            if (JSON.stringify(game.players[game.players.length - 1]) === JSON.stringify(game.players[i])) {
+                game.gameover = 1;
+            }
+            // if (game.players[head].player.xcoord === game.players[i].player.xcoord && game.players[head].player.ycoord === game.players[i].player.ycoord) {
+            //     game.gameover = 1;
+            // }
+        }
+    }
+    if (!(game.players[head].player.xcoord === game.apple.xcoord && game.players[head].player.ycoord === game.apple.ycoord)) {
+        game.players.shift();
+
+    }
+    else {
+        game.apple.xcoord = getRandomIntInclusive(game.border.w + 1, ctx.canvas.width - 1 - game.border.w);
+        game.apple.ycoord = getRandomIntInclusive(game.border.h + 1, ctx.canvas.height - 1 - game.border.h);
+    }
 }
-// do {
-//     setTimeout(
-//      modifyGameState(game)
-//      ,1000);
-//
-// //    updateGameState(game);
-// //    printGameState(game);
-// } while (!game.gameover);
 
 // end game grafix ("Game over")
 
@@ -108,6 +119,14 @@ function gameLoop(game) {
     updateGameState(game);
     printGameState(game);
     if (!game.gameover) {
-        setTimeout(gameLoop.bind(null,game), 500);
+        setTimeout(gameLoop.bind(null, game), 50);
     }
+    else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+}
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
